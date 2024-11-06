7c7145fbb2cc63573a4ac5ae132fcd985e09e585
dflynjeri <dflynjeri@gmail.com>
2024-11-06 04:24:27 +0300

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,12 +3,13 @@
 Function to determine if all boxes can be opened.
 """
 
+
 def canUnlockAll(boxes):
     """Determines if all boxes can be opened starting from the first box.
-    
+
     Args:
         boxes (list of lists): Each box contains keys to other boxes.
-        
+
     Returns:
         bool: True if all boxes can be opened, otherwise False.
     """
@@ -20,13 +21,13 @@ def canUnlockAll(boxes):
     # Process boxes until there are no more keys to use
     while stack:
         current_box = stack.pop()  # Take the last box added to the stack
-        
+
         for key in boxes[current_box]:
-            # If the key unlocks a new box, mark it as unlocked and add it to the stack
+            # If the key unlocks a new box, mark it as unlocked and add it to
+            # the stack
             if key not in unlocked and key < len(boxes):
                 unlocked.add(key)
                 stack.append(key)
 
     # All boxes are unlocked if we have `n` boxes in the `unlocked` set
     return len(unlocked) == len(boxes)
-
